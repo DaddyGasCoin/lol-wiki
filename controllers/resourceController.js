b5c8e5bfc626baa31c79f99790e1773fa4a0f72f
Michael Jones <iammichaeljon27@gmail.com>
2022-10-11 18:28:14 +0400

    Add functions and view to create resource

--- controllers/resourceController.js
+++ controllers/resourceController.js
@@ -1,5 +1,6 @@
 const Resource = require('../models/resourcetype')
 const Champion = require('../models/champions')
+const { body, validationResult } = require("express-validator");
 
 // Display list of all resources.
 exports.resource_list = (req, res) => {
@@ -17,7 +18,6 @@ exports.resource_list = (req, res) => {
 
 // Display detail page for a specific resource.
 exports.resource_detail = (req, res) => {
-   // res.send(`NOT IMPLEMENTED: resource detail: ${req.params.id}`);
    (async () => {
       const name = await Resource.findById(req.params.id)
       Champion.find({ resourcetype: name }, 'name key')
@@ -30,13 +30,53 @@ exports.resource_detail = (req, res) => {
 
 // Display resource create form on GET.
 exports.resource_create_get = (req, res) => {
-   res.send("NOT IMPLEMENTED: resource create GET");
+   res.render('resource_form', { title: 'Create Resource' })
 };
 
 // Handle resource create on POST.
-exports.resource_create_post = (req, res) => {
-   res.send("NOT IMPLEMENTED: resource create POST");
-};
+exports.resource_create_post = [
+   // Validate and sanitize the name field.
+   body("name", "Resource name required").trim().isLength({ min: 1 }).escape(),
+
+   // Process request after validation and sanitization.
+   (req, res, next) => {
+      // Extract the validation errors from a request.
+      const errors = validationResult(req);
+
+      // Create a genre object with escaped and trimmed data.
+      const resource = new Resource({ name: req.body.name });
+
+      if (!errors.isEmpty()) {
+         // There are errors. Render the form again with sanitized values/error messages.
+         res.render("resource_form", {
+            title: "Create Genre",
+            errors: errors.array(),
+         });
+         return;
+      } else {
+         // Data from form is valid.
+         // Check if resource with same name already exists.
+         Resource.findOne({ name: req.body.name }).exec((err, found_resource) => {
+            if (err) {
+               return next(err);
+            }
+
+            if (found_resource) {
+               // resource exists, redirect to its detail page.
+               res.redirect(found_resource.url);
+            } else {
+               resource.save((err) => {
+                  if (err) {
+                     return next(err);
+                  }
+                  // Resoource saved. Redirect to resource detail page.
+                  res.redirect(resource.url);
+               });
+            }
+         });
+      }
+   },
+];
 
 // Display resource delete form on GET.
 exports.resource_delete_get = (req, res) => {
