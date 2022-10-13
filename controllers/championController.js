68a6dbbfa5b99f904b8a90067b639f598641027e
Michael Jones <iammichaeljon27@gmail.com>
2022-10-13 23:04:56 +0400

    Add function to update champion

--- controllers/championController.js
+++ controllers/championController.js
@@ -231,7 +231,6 @@ exports.champion_create_post = [
 
       if (!errors.isEmpty()) {
          // There are errors. Render form again with sanitized values/error messages.
-
          // Get all class types and resorces for form.
          async.parallel(
             {
@@ -287,10 +286,265 @@ exports.champion_delete_post = (req, res) => {
 
 // Display champion update form on GET.
 exports.champion_update_get = (req, res) => {
-   res.send("NOT IMPLEMENTED: champion update GET");
+   // res.send("NOT IMPLEMENTED: champion update GET");
+   async.parallel(
+      {
+         champion(callback) {
+            Champion.findById(req.params.id)
+               .populate("class")
+               .populate("resourcetype")
+               .exec(callback);
+         },
+         classes(callback) {
+            Class.find(callback);
+         },
+         resources(callback) {
+            Resource.find(callback);
+         },
+      },
+      (err, results) => {
+         if (err) {
+            return next(err);
+         }
+         if (results.champion == null) {
+            // No results.
+            const err = new Error("Champion not found");
+            err.status = 404;
+            return next(err);
+         }
+         // Success.
+         // Mark our selected class types as checked.
+         for (const Class of results.classes) {
+            for (const champclass of results.champion.class) {
+               if (Class._id.toString() == champclass._id.toString())
+                  Class.checked = 'true'
+            }
+         }
+         res.render("champ_form", {
+            title: "Update Champion",
+            classes: results.classes,
+            resources: results.resources,
+            champion: results.champion
+         });
+      }
+   );
 };
 
 // Handle champion update on POST.
-exports.champion_update_post = (req, res) => {
-   res.send("NOT IMPLEMENTED: champion update POST");
-};
+exports.champion_update_post = [
+   // Convert the classes to an array
+   (req, res, next) => {
+      if (!Array.isArray(req.body.class)) {
+         req.body.class =
+            typeof req.body.class === "undefined" ? [] : [req.body.class];
+      }
+      next();
+   },
+   // Validate and sanitize fields.
+   body("name", "Name must not be empty.")
+      .trim()
+      .isLength({ min: 1 })
+      .escape(),
+   body("title", "title must not be empty.")
+      .trim()
+      .isLength({ min: 1 })
+      .escape(),
+   body("lore", "Lore must not be empty.")
+      .trim()
+      .isLength({ min: 1 })
+      .escape(),
+   body("attack", "Attack must not be empty.")
+      .trim()
+      .isLength({ min: 1 })
+      .escape(),
+   body("magic", "Magic must not be empty.")
+      .trim()
+      .isLength({ min: 1 })
+      .escape(),
+   body("defense", "Defense must not be empty.")
+      .trim()
+      .isLength({ min: 1 })
+      .escape(),
+   body("difficulty", "Difficuly must not be empty.")
+      .trim()
+      .isLength({ min: 1 })
+      .escape(),
+   body("hp", "HP must not be empty.")
+      .trim()
+      .isLength({ min: 1 })
+      .escape(),
+   body("hpperlevel", "HP per level must not be empty.")
+      .trim()
+      .isLength({ min: 1 })
+      .escape(),
+   body("mp", "Mana must not be empty.")
+      .trim()
+      .isLength({ min: 1 })
+      .escape(),
+   body("mpperlevel", "Mana per level must not be empty.")
+      .trim()
+      .isLength({ min: 1 })
+      .escape(),
+   body("movespeed", "Move speed must not be empty.")
+      .trim()
+      .isLength({ min: 1 })
+      .escape(),
+   body("armor", "Armor must not be empty.")
+      .trim()
+      .isLength({ min: 1 })
+      .escape(),
+   body("armorperlevel", "Armor must not be empty.")
+      .trim()
+      .isLength({ min: 1 })
+      .escape(),
+   body("mr", "Magic Resist must not be empty.")
+      .trim()
+      .isLength({ min: 1 })
+      .escape(),
+   body("mrperlevel", "Name must not be empty.")
+      .trim()
+      .isLength({ min: 1 })
+      .escape(),
+   body("attackrange", "Attack range must not be empty.")
+      .trim()
+      .isLength({ min: 1 })
+      .escape(),
+   body("hpregen", "HP regen  must not be empty.")
+      .trim()
+      .isLength({ min: 1 })
+      .escape(),
+   body("hpregenperlevel", "Hp regen per level must not be empty.")
+      .trim()
+      .isLength({ min: 1 })
+      .escape(),
+   body("mpregen", "Mana regen must not be empty.")
+      .trim()
+      .isLength({ min: 1 })
+      .escape(),
+   body("mpregenperlevel", "Mana regen per level must not be empty.")
+      .trim()
+      .isLength({ min: 1 })
+      .escape(),
+   body("crit", "Crit must not be empty.")
+      .trim()
+      .isLength({ min: 1 })
+      .escape(),
+   body("critperlevel", "Crit per level must not be empty.")
+      .trim()
+      .isLength({ min: 1 })
+      .escape(),
+   body("attackdamage", "Attack damage must not be empty.")
+      .trim()
+      .isLength({ min: 1 })
+      .escape(),
+   body("attackdamageperlevel", "Attack damage per level must not be empty.")
+      .trim()
+      .isLength({ min: 1 })
+      .escape(),
+   body("attackspeed", "Attack speed must not be empty.")
+      .trim()
+      .isLength({ min: 1 })
+      .escape(),
+   body("attackspeedperlevel", "Attack speed per level must not be empty.")
+      .trim()
+      .isLength({ min: 1 })
+      .escape(),
+   body("resourcetype", "Resource  must not be empty.")
+      .trim()
+      .isLength({ min: 1 })
+      .escape(),
+   body("class.*").escape(),
+   body("allytip.*").escape(),
+   body("enemytip.*").escape(),
+
+   // Process request after validation and sanitization.
+   (req, res, next) => {
+      // Extract the validation errors from a request.
+      const errors = validationResult(req);
+
+      // Create a champion object with escaped/trimmed data and old id.
+      const champion = new Champion({
+         name: req.body.name,
+         key: req.body.name,
+         title: req.body.title,
+         lore: req.body.lore,
+         class: req.body.class,
+         resourcetype: req.body.resourcetype,
+         allytips: [req.body.allytip1, req.body.allytip2, req.body.allytip3],
+         enemytips: [req.body.enemytip1, req.body.enemytip2, req.body.enemytip3],
+         info: {
+            attack: req.body.attack,
+            defense: req.body.defense,
+            magic: req.body.magic,
+            difficulty: req.body.difficulty,
+         },
+         stats: {
+            hp: req.body.hp,
+            hpperlevel: req.body.hpperlevel,
+            mp: req.body.mp,
+            mpperlevel: req.body.mpperlevel,
+            movespeed: req.body.movespeed,
+            armor: req.body.armor,
+            armorperlevel: req.body.armor,
+            spellblock: req.body.mr,
+            spellblockperlevel: req.body.mrperlevel,
+            attackrange: req.body.attackrange,
+            hpregen: req.body.hpregen,
+            hpregenperlevel: req.body.hpregenperlevel,
+            mpregen: req.body.mpregen,
+            mpregenperlevel: req.body.mpregenperlevel,
+            crit: req.body.crit,
+            critperlevel: req.body.critperlevel,
+            attackdamage: req.body.attackdamage,
+            attackdamageperlevel: req.body.attackdamageperlevel,
+            attackspeedperlevel: req.body.attackspeedperlevel,
+            attackspeed: req.body.attackspeed,
+         },
+         _id: req.params.id, //This is required, or a new ID will be assigned!
+      });
+      if (!errors.isEmpty()) {
+         // There are errors. Render form again with sanitized values/error messages.
+         // Get all class and resource types for form.
+         async.parallel(
+            {
+               classes(callback) {
+                  Class.find(callback);
+               },
+               resources(callback) {
+                  Resource.find(callback);
+               },
+            },
+            (err, results) => {
+               if (err) {
+                  return next(err);
+               }
+
+               // Mark our selected genres as checked.
+               for (const Class of results.classes) {
+                  if (champion.class.includes(Class._id)) {
+                     Class.checked = "true";
+                  }
+               }
+               res.render("champ_form", {
+                  title: "Update Champion",
+                  classes: results.classes,
+                  resources: results.resources,
+                  champion: results.champion,
+                  errors: errors.array(),
+               });
+            }
+         );
+         return;
+      }
+
+      // Data from form is valid. Update the record.
+      Champion.findByIdAndUpdate(req.params.id, champion, {}, (err, thechamp) => {
+         if (err) {
+            return next(err);
+         }
+
+         // Successful: redirect to book detail page.
+         res.redirect(thechamp.url);
+      });
+   },
+];
